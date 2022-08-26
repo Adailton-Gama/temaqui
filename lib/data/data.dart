1894dfc0ebf81371dcfd74cdd6e461365886f73c
Adailton Gama Santana <76784673+Adailton-Gama@users.noreply.github.com>
2022-08-26 00:25:58 -0300

    Version: 1.0.6

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -9,5 +9,48 @@ class Planos {
 class Categorias {
   String nome;
   String img;
-  Categorias({required this.nome, required this.img});
+  String subCategoria;
+  String imgSubCategoria;
+  Categorias({
+    required this.nome,
+    required this.img,
+    this.imgSubCategoria = '',
+    this.subCategoria = '',
+  });
+}
+
+class Profissional {
+  String nomeCompleto;
+  String telefone;
+  String cpf;
+  String endereco;
+
+  //
+  String categoria;
+  //
+  //Adicionar Sub Categoria
+
+  //Descrição da atividade
+  //Fotos
+  //Vídeos
+  //Redes Sociais
+  String profissao;
+
+  //
+  String plano;
+  //
+  bool autorizado;
+
+  //Adicionar validação se estiver pago
+
+  Profissional({
+    required this.nomeCompleto,
+    required this.telefone,
+    required this.cpf,
+    required this.endereco,
+    required this.categoria,
+    required this.profissao,
+    required this.plano,
+    this.autorizado = false,
+  });
 }
